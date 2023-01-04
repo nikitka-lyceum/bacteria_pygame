912c1b9a5f286ddf5bfe3a6d0a9ca85e7ec2d154
olesyakrysik <118808037+olesyakrysik@users.noreply.github.com>
2023-01-04 13:28:35 +0700

    Add files via upload
    Переделала то, что накосячила

--- server.py
+++ server.py
@@ -1,230 +1,209 @@
+import copy
 import json
 import socket
-import sys
-
-import pygame
+import threading
 from pygame import *
+import pygame.sprite
+
 from config import *
 
-from classes import *
+ip = socket.gethostbyname(socket.gethostname())
+print(ip)
 
-
-eat_image = pygame.image.load(PATH_IMAGE + "eat.png")
-eat_image = pygame.transform.scale(eat_image, (EAT_SIZE, EAT_SIZE))
-
-player_x, player_y, player_size = 0, 0, 100
-
-isLive = -1
-
-scale = 1
-last_size = 0
-
-camera = Camera()
+server_works = True
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+server.bind((ip, 2600))
+server.setblocking(False)
+server.listen()
+print("Server Working...")
 
 pygame.init()
-pygame.display.set_caption("Bacterium")
-screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
-grid = Grid(screen)
+pygame.display.set_caption("Server")
+screen = pygame.display.set_mode((500, 500), pygame.RESIZABLE)
 clock = pygame.time.Clock()
 
-def terminate():
-    pygame.quit()
-    sys.exit()
+map_objects = []
 
+timer = 0
 
-def die_screen(screen):
-    intro_text = [f"Вы умерли"]
+def new_users():
+    global map_objects
 
-    running = True
-    while running:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                terminate()
-
-            elif event.type == pygame.KEYDOWN or \
-                 event.type == pygame.MOUSEBUTTONDOWN:
-                running = False
-
-
-        screen.fill(BACKGROUND_COLOR)
-
-        # fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
-        # screen.blit(fon, (0, 0))
-        font = pygame.font.Font(None, 30)
-        text_coord = 50
-        for line in intro_text:
-            string_rendered = font.render(line, True, pygame.Color('white'))
-            intro_rect = string_rendered.get_rect()
-            text_coord += 10
-            intro_rect.top = text_coord
-            intro_rect.x = 10
-            text_coord += intro_rect.height
-            screen.blit(string_rendered, intro_rect)
-
-        pygame.display.update()
-
-    main()
-
-
-def start_screen(screen):
-    global isLive
-
-    isLive = 1
-    intro_text = ["ЗАСТАВКА", "",
-                  "Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
-
-    # fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
-    # screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
-    text_coord = 50
-    for line in intro_text:
-        string_rendered = font.render(line, True, pygame.Color('white'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
-                return
-
-        pygame.display.flip()
+    while server_works:
         clock.tick(FPS)
 
+        map_objects = sorted(map_objects, key=lambda x: str(x) == "Player", reverse=True)
 
-def draw(screen, visible):
-    pygame.display.set_caption(f"{player_x}, {player_y}")
-
-    screen.fill(BACKGROUND_COLOR)
-    grid.draww()
-
-    # Draw grid
-
-
-    # Draw self
-    screen.blit(pygame.transform.scale(pygame.image.load(PATH_IMAGE + "bacterium.png"),
-                                       (player_size / scale, player_size / scale)),
-                (WIDTH // 2 - player_size / scale // 2, HEIGHT // 2 - player_size / scale // 2))
-
-    # Draw info
-    font = pygame.font.Font(None, 20)
-    size_text = font.render(f"Размер: {player_size}", True, (20, 255, 35))
-    screen.blit(size_text, (WIDTH - size_text.get_width() - 5, 5))
-
-
-
-
-    for i in visible:
-        camera.update(player_x, player_y, player_size / scale, player_size / scale, WIDTH, HEIGHT)
-        if i["type_obj"] == "Player":
-            x = i["x"]
-            y = i["y"]
-            size = i["size"]
-            x, y = camera.apply(x, y)
-
-            screen.blit(pygame.transform.scale(pygame.image.load(PATH_IMAGE + "bacterium.png"), (size, size)), (x, y))
-
-        else:
-            x = i["x"]
-            y = i["y"]
-            x, y = camera.apply(player_x + x, player_y + y)
-            color = tuple(map(int, i["color"].strip("()").split(', ')))
-
-            eat_image_copy = eat_image.copy()
-            eat_image_copy.fill(color)
-            screen.blit(pygame.transform.scale(eat_image_copy, (EAT_SIZE / scale, EAT_SIZE / scale)), (x, y))
-
-
-    # Update display
-    pygame.display.update()
-
-
-def main():
-    global player_image, player_x, player_y, player_size, scale, last_size, isLive, WIDTH, HEIGHT
-
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-    client.settimeout(2)
-    client.connect((socket.gethostbyname(socket.gethostname()), 2600))
-
-    client.send(f"{WIDTH};{HEIGHT}".encode("utf-8"))
-
-    running = True
-    while running:
-        clock.tick(FPS)
-
-        # Update grid position
-        grid.update(player_x, player_y, scale)
-
-
-        # Check event
-        keys = pygame.key.get_pressed()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT or keys[K_ESCAPE]:
-                running = False
-                terminate()
-
+        # Connect new player
         try:
-            # Send client data
-            send_data = [
-                {"left": int(keys[K_a] or keys[K_LEFT]),
-                 "right": int(keys[K_d] or keys[K_RIGHT]),
-                 "up": int(keys[K_w] or keys[K_UP]),
-                 "down": int(keys[K_s] or keys[K_DOWN]),
-                 "radius_review": f"{WIDTH};{HEIGHT}"}
-            ]
-            client.send(f"{send_data}".encode("utf-8"))
+            client_socket, address = server.accept()
+            client_socket.setblocking(True)
 
+            map_objects.append(Player(sock=client_socket,
+                                      address=address,
+                                      x=random.randint(0, WORLD_WIDTH),
+                                      y=random.randint(0, WORLD_HEIGHT)))
 
-            # Apply server data
-            server_data = json.loads(client.recv(4 ** 10).decode("utf-8").strip("[]").replace("'", '"'))
-            visibles = server_data["visibles"]
-            player_x = server_data["x"]
-            player_y = server_data["y"]
-            player_size = server_data["size"]
-            isLive = server_data["isLive"]
-            scale = server_data["scale"]
-
-            if isLive == 0:
-                running = False
-
-            # Draw world
-            draw(screen, visibles)
-
-
-
-
-
-
-        except socket.timeout:
-            pass
+            print(f"Присоеденился {address}")
 
         except Exception:
-            terminate()
+            pass
 
-        player_x = WIDTH // 2 - player_x
-        player_y = HEIGHT // 2 - player_y
 
-        WIDTH, HEIGHT = screen.get_width(), screen.get_height()
+def update_timer():
+    global timer, map_objects
 
-        # Update camera
+    while server_works:
+        clock.tick(FPS)
 
-        camera.update(player_x, player_y, player_size / scale, player_size / scale, WIDTH, HEIGHT)
+        if timer >= 1 and [str(i) for i in map_objects].count("Eat") + 1 <= 150:
+            timer = 0
+            for _ in range(random.randint(1, 20)):
+                map_objects.append(Eat(color=(random.randint(0, 255),
+                                              random.randint(0, 255),
+                                              random.randint(0, 255))))
 
-    client.close()
-    die_screen(screen)
+        else:
+            timer += 1
 
-if __name__ == '__main__':
-    start_screen(screen)
-    main()
-    terminate()
+
+threading.Thread(target=new_users).start()
+threading.Thread(target=update_timer).start()
+
+while server_works:
+    clock.tick(FPS)
+
+    # Handler command
+    for obj in map_objects:
+        if str(obj) == "Player":
+            try:
+                keys = json.loads(obj.sock.recv(2 ** 10).decode("utf-8").strip("[]").replace("'", '"'))
+
+                obj.radius_review_x = int(keys["radius_review"].split(";")[0]) * obj.scale
+                obj.radius_review_y = int(keys["radius_review"].split(";")[1]) * obj.scale
+                obj.WIDTH = obj.radius_review_x
+                obj.HEIGHT = obj.radius_review_y
+
+                print(obj.radius_review_x)
+                print(obj.radius_review_y)
+
+                if keys["left"]:
+                    obj.rect = obj.rect.move(-obj.speed, 0)
+
+                if keys["right"]:
+                    obj.rect = obj.rect.move(obj.speed, 0)
+
+                if keys["up"]:
+                    obj.rect = obj.rect.move(0, -obj.speed)
+
+                if keys["down"]:
+                    obj.rect = obj.rect.move(0, obj.speed)
+
+            except Exception:
+                pass
+
+    # Find visible enemy
+    visibles = [[] for _ in range(len(map_objects))]
+
+    for i in range(len(map_objects)):
+        for j in range(len(map_objects)):
+            if str(map_objects[i]) == "Player" and map_objects[i] != map_objects[j]:
+                dict_x = map_objects[i].rect.x - map_objects[j].rect.x
+                dict_y = map_objects[i].rect.y - map_objects[j].rect.y
+
+                if abs(dict_x) <= map_objects[i].radius_review_x and abs(dict_y) <= map_objects[i].radius_review_y:
+                    type_obj = str(map_objects[j])
+
+                    x = round(dict_x / map_objects[i].scale) * -1
+                    y = round(dict_y / map_objects[i].scale) * -1
+
+                    if type_obj == "Player":
+                        size = round(map_objects[j].force // map_objects[i].scale)
+                        j_data = {
+                            'type_obj': type_obj,
+                            'nickname': map_objects[j].name,
+                            'x': x,
+                            'y': y,
+                            'size': size
+                        }
+                        visibles[i].append(j_data)
+
+                    else:
+                        color = str(map_objects[j].color)
+                        j_data = {
+                            'type_obj': type_obj,
+                            'x': x,
+                            'y': y,
+                            'color': color
+                        }
+                        visibles[i].append(j_data)
+
+    # Check collide
+    for i in range(len(map_objects)):
+        for j in range(i + 1, len(map_objects)):
+            try:
+                if str(map_objects[i]) == "Player":
+                    if map_objects[i].rect.colliderect(map_objects[j].rect):
+
+                        if str(map_objects[j]) == "Player":
+                            if map_objects[i].force > map_objects[j].force:
+                                map_objects[i].force += map_objects[j].force // 5
+                                map_objects[j].isLive = 0
+
+                            elif map_objects[i].force < map_objects[j].force:
+                                map_objects[j].force += map_objects[i].force // 5
+                                map_objects[i].isLive = 0
+
+                        else:
+                            map_objects[i].force += map_objects[j].force
+                            map_objects.remove(map_objects[j])
+
+            except Exception:
+                pass
+
+    # Send server data
+    for i in range(len(map_objects)):
+        try:
+            map_objects[i].error = 0
+
+            if str(map_objects[i]) == "Player":
+
+                server_data = [
+                    {'x': map_objects[i].rect.x,
+                     'y': map_objects[i].rect.y,
+                     'size': round(map_objects[i].force),
+                     'name': map_objects[i].name,
+                     'scale': map_objects[i].scale,
+                     'isLive': map_objects[i].isLive,
+                     'visibles': visibles[i]}
+                ]
+                map_objects[i].sock.send(f"{server_data}".encode("utf-8"))
+
+                if map_objects[i].isLive == 0:
+                    pass
+
+        except Exception:
+            try:
+                map_objects[i].sock.close()
+                map_objects.remove(map_objects[i])
+            except Exception:
+                pass
+
+    # Check event
+    for event in pygame.event.get():
+        keys = pygame.key.get_pressed()
+        if event.type == pygame.QUIT or keys[K_ESCAPE]:
+            server_works = False
+
+    # Draw screen
+    screen.fill(BACKGROUND_COLOR)
+
+    for obj in map_objects:
+        obj.draw(screen)
+        obj.update(obj.WIDTH, obj.HEIGHT)
+
+    pygame.display.update()
+
+pygame.quit()
+server.close()
